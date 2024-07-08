11390f58e8b32f46d1d768fe5c33502050ef3f56
Issac Shaik <issac.shaik@gmail.com>
2024-07-08 15:19:07 +0530

    ADD auth

---
+++ app/components/UserNav.tsx
@@ -0,0 +1,32 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export function UserNav() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-10 w-10 rounded-full">
+          <Avatar className="h-14 w-14">
+            <AvatarFallback>Anon</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">Issac Shaik</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              issac.shaik@gmail.com
+            </p>
+          </div>
+        </DropdownMenuLabel>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
